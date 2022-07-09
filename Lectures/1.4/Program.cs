98fb4cac640af62cf5dfbe8d01a42f1a17419011
IgorRoma <romigole@gmail.com>
2022-07-09 14:10:13 +0300

    Lecture_1.4

---
+++ Lectures/1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, it's Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
