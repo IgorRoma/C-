0769f9249a3dff823402226657f0aefcd3e8c826
IgorRoma <romigole@gmail.com>
2022-07-10 19:14:25 +0300

    Task_4

---
+++ Practice/Hw_1/T_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите 3 различных числа для определения максимального: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
