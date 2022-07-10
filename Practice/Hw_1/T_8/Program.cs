d25e7a7940ee745274e6bbe0233643b1c0c585fd
IgorRoma <romigole@gmail.com>
2022-07-10 20:21:17 +0300

    Task_8

---
+++ Practice/Hw_1/T_8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает 
+//число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите положительное число N: ");
+double n = Convert.ToDouble(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + "  "); 
+    }
+}
