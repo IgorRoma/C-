5eef1f004113e862575419cb9dfc6de4e8c5a2c5
IgorRoma <romigole@gmail.com>
2022-07-18 21:26:54 +0300

    Task_13

---
+++ Practice/Hw_2/T_13/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет. 
+
+Console.Clear();
+
+Console.WriteLine("Введите целое число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Three() // третье число с проверкой, на знак и отсутствие
+{
+    if (n > 0)
+    {}
+    else
+    {n = -n;}
+    Console.Write("Третье число: ");
+    Console.WriteLine(n>99 ? n.ToString()[2] : "отсутствует");
+    Console.ReadKey(true);
+}
+
+Three(); 
