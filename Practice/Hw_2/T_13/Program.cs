576a2eaab186c32bd6a7f2ce45d51f3b190b9072
IgorRoma <romigole@gmail.com>
2022-07-18 22:51:28 +0300

    Task_13

--- Practice/Hw_2/T_13/Program.cs
+++ Practice/Hw_2/T_13/Program.cs
@@ -10,7 +10,7 @@ int n = Convert.ToInt32(Console.ReadLine());
 void Three() // третье число с проверкой, на знак и отсутствие
 {
     if (n > 0) {} else { n = -n; }
-    Console.Write("Третье число: ");
+    Console.Write("Третья цифра: ");
     Console.WriteLine(n>99 ? n.ToString()[2] : "отсутствует");
     Console.ReadKey(true);
 }
