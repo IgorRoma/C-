8be1a70e6f2cb7f002736f2778d3855cacb9dbce
IgorRoma <romigole@gmail.com>
2022-08-07 18:57:54 +0300

    Lecture_3.2

---
+++ Lectures/3.2/Program.cs
@@ -0,0 +1,40 @@
+﻿//FOR
+Console.Clear();
+
+/*string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(3, "W");
+Console.WriteLine(res);*/
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(3, "W");
+//Console.WriteLine(res);
+
+for (int i = 2; i <=10; i++)
+{
+    for (int j = 2; j <=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
