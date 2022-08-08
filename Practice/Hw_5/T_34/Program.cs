e109dfa3fc6a89a93a296a0fe33020f727f26d0e
IgorRoma <romigole@gmail.com>
2022-08-08 20:09:08 +0300

    Task_34

---
+++ Practice/Hw_5/T_34/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+
+int[] num = new int[4];
+
+void FillArray(int[] array, int min, int max)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int PosNum(int[] array)
+{
+    int posn = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            posn++;
+        }
+    }
+    return posn;
+}
+
+FillArray(num, 100, 1000);
+WriteArray(num);
+Console.WriteLine();
+
+int posn = PosNum(num);
+Console.WriteLine($"Количество чётных чисел в массиве: {posn}");
